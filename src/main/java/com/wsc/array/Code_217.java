cfc52d3611f432bcdf82d8ac001842df5e4281f4
WangS-C <17775725837@163.com>
2023-12-08 23:11:02 +0800

    update: 1: 存在重复元素 https://leetcode.cn/problems/contains-duplicate/description/

--- src/main/java/com/wsc/array/Code_217.java
+++ src/main/java/com/wsc/array/Code_217.java
@@ -1,17 +1,28 @@
 package com.wsc.array;
 
+import java.util.Arrays;
+
 public class Code_217 {
     public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int size = nums.length-1;
+        for (int i = 0; i < size; i++) {
+            if (nums[i]==nums[i+1]){
+                return true;
+            }
+        }
 
-        return true;
-
+        return false;
     }
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5, 6, 7};
+        int[] arr1 = {1,2,3,1};
         System.out.println(containsDuplicate(arr1));
 
-        int[] arr2 = {-1, -100, 3, 99};
+        int[] arr2 = {1,2,3,4};
         System.out.println(containsDuplicate(arr2));
+
+        int[] arr3 = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(arr3));
     }
 }
