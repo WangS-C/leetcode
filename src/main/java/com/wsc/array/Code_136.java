c45002d70d326592674c57719212ab8619b2f1b8
WangS-C <17775725837@163.com>
2023-12-11 21:54:29 +0800

    update: 1: 数组异或方面的题目 https://leetcode.cn/problems/single-number/description/
    https://leetcode.cn/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/

---
+++ src/main/java/com/wsc/array/Code_136.java
@@ -0,0 +1,21 @@
+package com.wsc.array;
+
+public class Code_136 {
+    public static int singleNumber(int[] nums) {
+
+        return  0 ;
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr1 = {2,2,1};
+        System.out.println(singleNumber(arr1));
+
+        int[] arr2 = {4,1,2,1,2};
+        System.out.println(singleNumber(arr2));
+
+        int[] arr3 = {1};
+        System.out.println(singleNumber(arr3));
+    }
+}
---
+++ src/main/java/com/wsc/array/Code_177.java
@@ -0,0 +1,16 @@
+package com.wsc.array;
+
+public class Code_177 {
+    public static int[] sockCollocation(int[] sockets) {
+
+        return sockets;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {4, 5, 2, 4, 6, 6};
+        System.out.println(sockCollocation(arr1));
+
+        int[] arr2 = {1, 2, 4, 1, 4, 3, 12, 3};
+        System.out.println(sockCollocation(arr2));
+    }
+}
