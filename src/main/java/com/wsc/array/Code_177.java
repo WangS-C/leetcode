010506b3d5b4c175f9e87ddbebe274e0a099afc7
WangS-C <17775725837@163.com>
2023-12-12 21:47:11 +0800

    update: 1: 撞色搭配 https://leetcode.cn/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/description/

--- src/main/java/com/wsc/array/Code_177.java
+++ src/main/java/com/wsc/array/Code_177.java
@@ -1,16 +1,37 @@
 package com.wsc.array;
 
+import java.util.Arrays;
+
 public class Code_177 {
     public static int[] sockCollocation(int[] sockets) {
+        int i = 0;
+        for (int socket : sockets) {
+            i = socket ^ i;
+        }
+        System.out.println(Integer.toBinaryString(i));
 
-        return sockets;
+        int lowestBit = i & -i;
+        System.out.println(Integer.toBinaryString(lowestBit));
+
+        int a = 0, b = 0;
+        for (int socket : sockets) {
+            if ((socket & lowestBit) == 0) {
+                a = socket ^ a;
+            } else {
+                b = socket ^ b;
+            }
+        }
+
+        return new int[]{a, b};
     }
 
     public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(0));
+
         int[] arr1 = {4, 5, 2, 4, 6, 6};
-        System.out.println(sockCollocation(arr1));
+        System.out.println(Arrays.toString(sockCollocation(arr1)));
 
         int[] arr2 = {1, 2, 4, 1, 4, 3, 12, 3};
-        System.out.println(sockCollocation(arr2));
+        System.out.println(Arrays.toString(sockCollocation(arr2)));
     }
 }
