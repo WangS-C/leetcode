0b57d289a9ac356544f2311c00172a888deeaabe
WangS-C <17775725837@163.com>
2023-12-12 21:30:46 +0800

    update: 1: 只出现一次的数字 https://leetcode.cn/problems/single-number/description/

--- src/main/java/com/wsc/array/Code_136.java
+++ src/main/java/com/wsc/array/Code_136.java
@@ -2,17 +2,19 @@ package com.wsc.array;
 
 public class Code_136 {
     public static int singleNumber(int[] nums) {
-
-        return  0 ;
+        int i = 0;
+        for (int num : nums) {
+            i = num ^ i;
+        }
+        return i;
     }
 
 
-
     public static void main(String[] args) {
-        int[] arr1 = {2,2,1};
+        int[] arr1 = {2, 2, 1};
         System.out.println(singleNumber(arr1));
 
-        int[] arr2 = {4,1,2,1,2};
+        int[] arr2 = {4, 1, 2, 1, 2};
         System.out.println(singleNumber(arr2));
 
         int[] arr3 = {1};
