ad2a16797b04666a3f88ac78db3d5ad4031233be
WangS-C <17775725837@163.com>
2023-12-19 22:36:38 +0800

    update: 1: 俩数之和
    https://leetcode.cn/problems/two-sum/

---
+++ src/main/java/com/wsc/array/Code_1.java
@@ -0,0 +1,23 @@
+package com.wsc.array;
+
+import java.util.Arrays;
+
+public class Code_1 {
+    public static int[] twoSum(int[] nums, int target) {
+
+        return  nums;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(arr1, 9)));
+
+        int[] arr2 = {3,2,4};
+        System.out.println(Arrays.toString(twoSum(arr2, 6)));
+
+        int[] arr3 = {3,3};
+        System.out.println(Arrays.toString(twoSum(arr3, 6)));
+    }
+}
