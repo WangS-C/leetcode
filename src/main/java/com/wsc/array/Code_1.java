68a672274b9a745e4f41d4ed7ae4ed0d7fb4f933
WangS-C <17775725837@163.com>
2023-12-20 22:56:34 +0800

    update: 1: 俩数之和
    https://leetcode.cn/problems/two-sum/

--- src/main/java/com/wsc/array/Code_1.java
+++ src/main/java/com/wsc/array/Code_1.java
@@ -1,23 +1,36 @@
 package com.wsc.array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Code_1 {
     public static int[] twoSum(int[] nums, int target) {
+        int length = nums.length;
+        Map<Integer, Integer> map = new HashMap<>(length);
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            int k = target - num;
+            Integer v = map.getOrDefault(k, null);
+            if (v != null) {
+                return new int[]{v, i};
+            }
+            map.put(num, i);
+        }
 
-        return  nums;
+        return new int[]{0};
     }
 
 
     public static void main(String[] args) {
 
-        int[] arr1 = {2,7,11,15};
+        int[] arr1 = {2, 7, 11, 15};
         System.out.println(Arrays.toString(twoSum(arr1, 9)));
 
-        int[] arr2 = {3,2,4};
+        int[] arr2 = {3, 2, 4};
         System.out.println(Arrays.toString(twoSum(arr2, 6)));
 
-        int[] arr3 = {3,3};
+        int[] arr3 = {3, 3};
         System.out.println(Arrays.toString(twoSum(arr3, 6)));
     }
 }
