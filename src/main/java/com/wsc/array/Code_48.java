26b1adf5ffbd35984ad4281ec0be4235a85e1368
WangS-C <17775725837@163.com>
2024-01-03 22:39:39 +0800

    update: 1: 旋转图像
    https://leetcode.cn/problems/rotate-image/

---
+++ src/main/java/com/wsc/array/Code_48.java
@@ -0,0 +1,18 @@
+package com.wsc.array;
+
+public class Code_48 {
+    public static void rotate(int[][] matrix) {
+
+    }
+
+
+    public static void main(String[] args) {
+        int[][] matrix1  = {{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix1);
+        System.out.println(matrix1);
+
+        int[][] matrix2  = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        rotate(matrix2);
+        System.out.println(matrix2);
+    }
+}
